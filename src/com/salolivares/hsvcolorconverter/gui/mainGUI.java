f76c544f934b4a4230ddf2123d7e26710b3e58e3
Sal Olivares <salisonhismac@gmail.com>
2015-08-08 16:55:12 -0700

    add menubar with option to open a file and exit

--- src/com/salolivares/hsvcolorconverter/gui/mainGUI.java
+++ src/com/salolivares/hsvcolorconverter/gui/mainGUI.java
@@ -2,6 +2,7 @@ package com.salolivares.hsvcolorconverter.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class mainGUI extends JFrame{
 
@@ -17,6 +18,7 @@ public class mainGUI extends JFrame{
         openCVPanel openCV = new openCVPanel();
 
         // Create menu bar
+        createMenuBar();
 
         // Customize the stock java color picker
         colorC.setBorder(BorderFactory.createTitledBorder("Choose Color"));
@@ -28,11 +30,38 @@ public class mainGUI extends JFrame{
         // add color picker
         add(colorPicker, BorderLayout.PAGE_END);
 
-        // customize jframe behavior
+        // customize JFrame behavior
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         setLocationRelativeTo(null);
         setResizable(false);
         setVisible(true);
+    }
+
+    private void createMenuBar(){
+        // Declare JComponents used
+        JMenuBar menubar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem exitLabel = new JMenuItem("Exit");
+        JMenuItem openLabel = new JMenuItem("Open");
+
+        // add action listeners to labels
+        exitLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+                System.exit(0);
+            }
+        });
+
+        // add labels to file menu
+        fileMenu.add(openLabel);
+        fileMenu.addSeparator();
+        fileMenu.add(exitLabel);
+
+        menubar.add(fileMenu);
+
+        setJMenuBar(menubar);
+
     }
 }
